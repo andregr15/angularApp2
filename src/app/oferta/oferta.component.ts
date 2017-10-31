7ec89570d9f97e6bf143c269550dd6fff40b3629
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2017-10-31 08:30:55 -0200

    testes em oferta com observables

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Oferta } from '../shared/oferta.model';
 import { OfertasService } from '../ofertas.service';
+
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-oferta',
@@ -9,9 +14,12 @@ import { OfertasService } from '../ofertas.service';
   styleUrls: ['./oferta.component.css'],
   providers: [OfertasService]
 })
-export class OfertaComponent implements OnInit {
+export class OfertaComponent implements OnInit, OnDestroy {
 
   public oferta: Oferta;
+
+  private sub1: Subscription;
+  private sub2: Subscription;
 
   constructor(private ofertasService: OfertasService, private route: ActivatedRoute) { }
 
@@ -24,6 +32,38 @@ export class OfertaComponent implements OnInit {
           this.oferta = oferta;
         }).
         catch((param: any) => console.log(param));
+
+        // this.route.params.subscribe(
+        //   (parametro: any) => console.log(parametro),
+        //   (erro: any) => console.log(erro),
+        //   () => console.log('processamento foi classificado como concluído!')
+        // );
+
+        let tempo = Observable.interval(500);
+        this.sub1 = tempo.subscribe((intervalo: number) => {
+          console.log(intervalo);
+        });
+
+        // observable (observável)
+        let meuObservableTeste = Observable.create(
+          (observer: Observer<number>) => {
+            observer.next(1);
+            observer.next(2);
+            observer.complete();
+          }
+        );
+
+
+        // observable (observador)
+        this.sub2 = meuObservableTeste.subscribe(
+          (resultado: number) => { console.log(resultado); },
+          (erro: string) => { console.log(erro); },
+          () => { console.log('stream de eventos foi finalizada'); }
+        );
   }
 
+  ngOnDestroy() {
+    this.sub1.unsubscribe();
+    this.sub2.unsubscribe();
+  }
 }
